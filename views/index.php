d51002f238cbe8b8b4129a52dbde0d9e375a7247
Wayl Louaked <wlouaked@f4r7s14.clusters.42paris.fr>
2025-12-20 20:13:26 +0100

    Update Frontend

--- views/index.php
+++ views/index.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/images/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/images/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicon/favicon-16x16.png">
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="assets/css/index_main.css">
     <link rel="stylesheet" href="assets/css/footer.css">
@@ -14,26 +14,25 @@
 <body>
     <main>
         <div class="index_left">
-            <img src="assets//images/landing-2x.png" alt="Landing page image">
+            <img class = "landing img"src="assets//images/landing-2x.png" alt="Landing page image">
         </div>
         <div class="index_right">
             <div class="top">
-            <img src="assets//images/logo.png" alt="">
-            <input type="text" id ="input_login" placeholder="Email">
-            <input type="text" id ="input_password" placeholder="Password">
-            <button type="submit">Log in</button>
-                <div style="display: flex; align-items: center; text-align: center; margin: 20px 0;">
-                    <div style="flex: 1; border-bottom: 1px solid #ddd;"></div>
-                    <span style="padding: 0 10px; color: #666; font-weight: bold; ">OR</span>
-                    <div style="flex: 1; border-bottom: 1px solid #ddd;"></div>
-                </div>
+            <img src="assets//images/logo.png" alt="Camagru Logo">
+                <input type="text" id ="input_login" placeholder="Email">
+                <input type="text" id ="input_password" placeholder="Password">
+                <button type="submit">Log in</button>
+                    <div class="or" >
+                        <div class="left or" ></div>
+                            <span class="or">OR</span>
+                                <div class= "right or" ></div>
+                    </div>
             </div>
             <div class="bottom">
                 <p><a href="https://opensource.org/licenses/MIT" target="_blank"> Forgot password?</a></p>
                 <p>Don't have an account?<a href="index.php?action=register"> Sign up</a></p>
             </div>
         </div>
-
     </main>
     <footer>
         <p>© 2025 Wayl Louaked . This project is licensed under the 
--- views/register.php
+++ views/register.php
@@ -8,20 +8,28 @@
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="assets/css/footer.css">
     <link rel="stylesheet" href="assets/css/register.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/images/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/images/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicon/favicon-16x16.png">
 </head>
 <body>
     <main>
         <div class="top">
-            <h1>Register</h1>
+            <img src="assets//images/logo.png" alt="Camagru Logo">
+            <p>Sign up to see photos from your friends.</p>
             <form method="POST" action="index.php?action=register">
-                <input type="text" name="username" placeholder="Username" required>
                 <input type="email" name="email" placeholder="Email" required>
                 <input type="password" name="password" placeholder="Password" required>
-                <button type="submit">Register</button>
+                <input type="text" name="fullname" placeholder="Fullname" required>
+                <input type="text" name="username" placeholder="Username" required>
+                <button type="submit">Next</button>
             </form>
+            <p>People who use our service may have uploaded your contact information to Instagram.<a href="https://opensource.org/licenses/MIT" target="_blank"> Learn More</a></p>
+            <p>By signing up, you agree to our <a href="https://opensource.org/licenses/MIT" target="_blank">Terms</a>. Learn how we collect, use and share your data in our <a href="https://opensource.org/licenses/MIT" target="_blank">Privacy Policy</a> and how we use cookies and similar technology in our <a href="https://opensource.org/licenses/MIT" target="_blank">Cookies Policy</a>.</p>
         </div>
         <div class="bottom">
-            
+            <p>Have an account?</p>
+            <p><a href="https://opensource.org/licenses/MIT" target="_blank">Log in</a></p>
         </div>
     
     </main>
