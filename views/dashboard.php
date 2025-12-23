beb6a8864a2ed1a900ce739d7ca83a829afad86b
Wayl Louaked <wlouaked@f4r11s4.clusters.42paris.fr>
2025-12-23 23:30:28 +0100

    Update Frontend

--- views/dashboard.php
+++ views/dashboard.php
@@ -6,23 +6,50 @@
     <title>Camagru</title>
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="assets/css/footer.css">
-    <link rel="stylesheet" href="assets/css/register.css">
+    <link rel="stylesheet" href="assets/css/dashboard.css">
     <link rel="apple-touch-icon" sizes="180x180" href="assets/images/favicon/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="assets/images/favicon/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicon/favicon-16x16.png">
 </head>
 <body>
     <nav>
+        <img class="logo" src="assets//images/logo.png" alt="Camagru Logo">
         <ul>
-            <li><a href="https://opensource.org/licenses/MIT"></a>Home</li> <!-- Gallerie aleatoire plus acces nav -->
-            <li><a href="https://opensource.org/licenses/MIT"></a>Explore</li> <!-- Gallerie dans lordre de creation -->
-            <li><a href="https://opensource.org/licenses/MIT">Create</a></li> <!-- Creer une image-->
-            <li><a href="https://opensource.org/licenses/MIT">Profile</a></li> <!-- Account Setting-->
+            <li>
+                <a href="https://opensource.org/licenses/MIT">
+                    <img class="home icon" src="assets//images/home.svg" alt="home icon">
+                    <span>Home</span>
+                </a>
+            </li> 
+            <li>
+                <a href="https://opensource.org/licenses/MIT">
+                    <img class="search icon" src="assets//images/search.svg" alt="search icon">
+                    <span>Search</span>
+                </a>
+            </li>
+            <li>
+                <a href="https://opensource.org/licenses/MIT">
+                    <img class="explore icon" src="assets//images/compass.svg" alt="explore icon">
+                    <span>Explore</span>
+                </a>
+            </li>
+            <li>
+                <a href="https://opensource.org/licenses/MIT">
+                    <img class="create icon" src="assets//images/create.svg" alt="create icon">
+                    <span>Create</span>
+                </a>
+            </li>
+            <li>
+                <a href="https://opensource.org/licenses/MIT">
+                    <img class="profile icon" src="assets//images/profile.svg" alt="profile icon">
+                    <span>Profile</span>
+                </a>
+            </li>
         </ul>
     </nav>
     <main>
-
     </main>
+    <div class="corner"></div>
     <?php require_once "footer.php" ?>
 </body>
 </html>
