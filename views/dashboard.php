86631a90e28c78aad1d7b799be12ee69671498ba
Wayl <louakedwayl@protonmail.com>
2025-12-26 22:59:38 +0100

    Update Frontend

--- views/dashboard.php
+++ views/dashboard.php
@@ -18,41 +18,52 @@
         <ul>
             <li>
                 <a href="https://opensource.org/licenses/MIT">
-                    <img class="icon home" src="assets/images/icon/home.svg" alt="home icon">
-                    <span class = "icon home">Home</span>
+                    <span class="link-area">
+                        <img class="icon home" src="assets/images/icon/home.svg" alt="home icon">
+                        <span class = "icon home">Home</span>
+                    </span>
                 </a>
             </li> 
             <li>
                 <a href="https://opensource.org/licenses/MIT">
-                    <img class="icon search" src="assets/images/icon/search.svg" alt="search icon">
-                    <span class = "icon search">Search</span>
+                    <span class="link-area">
+                        <img class="icon search" src="assets/images/icon/search.svg" alt="search icon">
+                        <span class = "icon search">Search</span>
+                    </span>
                 </a>
             </li>
             <li>
                 <a href="https://opensource.org/licenses/MIT">
-                    <img class="icon explore" src="assets/images/icon/compass.svg" alt="explore icon">
-                    <span class = "icon explore">Explore</span>
-                </a>
-            </li>
-
-            <li>
-                <a href="https://opensource.org/licenses/MIT">
-                    <img class="icon notifications" src="assets/images/icon/heart.svg" alt="profile icon">
-                    <span class = "icon notifications">Notifications</span>
+                    <span class="link-area">
+                        <img class="icon explore" src="assets/images/icon/compass.svg" alt="explore icon">
+                        <span class = "icon explore">Explore</span>
+                    </span>
                 </a>
             </li>
 
             <li>
                 <a href="https://opensource.org/licenses/MIT">
-                    <img class="icon create" src="assets/images/icon/create.svg" alt="create icon">
-                    <span class = "icon create">Create</span>
+                    <span class="link-area">
+                        <img class="icon notifications" src="assets/images/icon/heart.svg" alt="profile icon">
+                        <span class = "icon notifications">Notifications</span>
+                    </span>
                 </a>
             </li>
 
             <li>
                 <a href="https://opensource.org/licenses/MIT">
-                    <img class="icon profile" src="assets/images/icon/profile.svg" alt="profile icon">
-                    <span class = "icon profile">Profile</span>
+                    <span class="link-area">
+                        <img class="icon create" src="assets/images/icon/create.svg" alt="create icon">
+                        <span class = "icon create">Create</span>
+                    </span>
+                </a>
+            </li>
+            <li>
+                <a href="https://opensource.org/licenses/MIT">
+                    <span class="link-area">
+                        <img class="icon profile" src="assets/images/icon/profile.svg" alt="profile icon">
+                        <span class = "icon profile">Profile</span>
+                    </span>
                 </a>
             </li>
         </ul>
@@ -60,6 +71,10 @@
     <main>
     </main>
     <div class="corner">
+        <a href="https://opensource.org/licenses/MIT" class = "icon more">
+            <img class="icon more" src="assets/images/icon/more.svg" alt="more icon">
+            <span class = "icon more">More</span>
+        </a>
     </div>
     <?php require_once "footer.php" ?>
 </body>
