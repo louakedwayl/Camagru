c8c2b85a84febae2030002e986425771f681a00c
Wayl <louakedwayl@protonmail.com>
2025-12-27 01:52:34 +0100

    Update frontend

--- views/dashboard.php
+++ views/dashboard.php
@@ -18,51 +18,51 @@
         <ul>
             <li>
                 <a href="https://opensource.org/licenses/MIT">
-                    <span class="link-area">
+                    <span class="link-area home">
                         <img class="icon home" src="assets/images/icon/home.svg" alt="home icon">
-                        <span class = "icon home">Home</span>
+                        <span class = "icon home nav">Home</span>
                     </span>
                 </a>
             </li> 
             <li>
                 <a href="https://opensource.org/licenses/MIT">
-                    <span class="link-area">
+                    <span class="link-area search">
                         <img class="icon search" src="assets/images/icon/search.svg" alt="search icon">
-                        <span class = "icon search">Search</span>
+                        <span class = "icon search nav">Search</span>
                     </span>
                 </a>
             </li>
             <li>
                 <a href="https://opensource.org/licenses/MIT">
-                    <span class="link-area">
+                    <span class="link-area explore">
                         <img class="icon explore" src="assets/images/icon/compass.svg" alt="explore icon">
-                        <span class = "icon explore">Explore</span>
+                        <span class = "icon explore nav">Explore</span>
                     </span>
                 </a>
             </li>
 
             <li>
                 <a href="https://opensource.org/licenses/MIT">
-                    <span class="link-area">
+                    <span class="link-area notifications">
                         <img class="icon notifications" src="assets/images/icon/heart.svg" alt="profile icon">
-                        <span class = "icon notifications">Notifications</span>
+                        <span class = "icon notifications nav">Notifications</span>
                     </span>
                 </a>
             </li>
 
             <li>
                 <a href="https://opensource.org/licenses/MIT">
-                    <span class="link-area">
+                    <span class="link-area create">
                         <img class="icon create" src="assets/images/icon/create.svg" alt="create icon">
-                        <span class = "icon create">Create</span>
+                        <span class = "icon create nav">Create</span>
                     </span>
                 </a>
             </li>
             <li>
                 <a href="https://opensource.org/licenses/MIT">
-                    <span class="link-area">
+                    <span class="link-area profile">
                         <img class="icon profile" src="assets/images/icon/profile.svg" alt="profile icon">
-                        <span class = "icon profile">Profile</span>
+                        <span class = "icon profile nav">Profile</span>
                     </span>
                 </a>
             </li>
