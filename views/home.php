7a86ab515b5cc5f63341dc8d2fccded5bc4f3bf9
Wayl Louaked <wlouaked@f4r13s12.clusters.42paris.fr>
2026-01-25 19:24:14 +0100

    Refactor: move home corner code into corner.php

--- views/home.php
+++ views/home.php
@@ -17,29 +17,7 @@
     <?php require_once __DIR__ . '/navbar.php'; ?>
     <main>
     </main>
-    <div class="corner">
-        <a href="#" class = "icon more">
-            <img class="icon more" src="assets/images/icon/more.svg" alt="more icon">
-            <span class = "icon more">More</span>
-        </a>
-          <div class="hamburger">
-            <ul>
-                <li>
-                    <a href="#" class= "report hamburger">
-                        <span class="link-area-hamburger report">
-                            <img class="icon report" src="assets/images/icon/report.svg" alt="report icon">
-                            <span class = "report">Report a problem</span>
-                        </span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" >
-                        <span class = "link-area-hamburger logout">Log out</span>
-                    </a>
-                </li>
-            </ul>
-        </div>
-    </div>
+    <?php require_once "corner.php" ?>
     <?php require_once "modale_report.php" ?>
     <?php require_once "footer.php" ?>
 </body>
