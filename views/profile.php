52c049f1d95e690f711a0d73bb1fb987e63070e3
Wayl Louaked <wlouaked@f6r13s14.clusters.42paris.fr>
2026-01-31 22:17:36 +0100

    Update views/profile.php

--- views/profile.php
+++ views/profile.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Camagru</title>
-    <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="assets/css/footer.css">
     <link rel="stylesheet" href="assets/css/profile.css">
     <link rel="apple-touch-icon" sizes="180x180" href="assets/images/favicon/apple-touch-icon.png">
@@ -14,9 +13,28 @@
     <script defer src="assets/js/profile.js"></script>
 </head>
 <body>
-    <div class="overlay"></div>
     <?php require_once __DIR__ . '/navbar.php'; ?>
-    <main></main>
+    <main>
+        <header>
+                <form id="avatar-form" enctype="multipart/form-data">
+                <label for="avatar-input" class="avatar-label">
+                <img src="assets/images/default-avatar.jpeg" class="profile-avatar" id="current-avatar">
+                <div class="avatar-overlay">
+                </div>
+                </label>
+                <input type="file" id="avatar-input" name="avatar" accept="image/*" style="display: none;">
+                </form>
+                <div class="user-info">
+                    <span class="fullname"><?php echo $user['full_name']?></span>
+                    <span class="username"><?php echo $user['username']?></span>
+                </div>
+        </header>
+        <div class="user-actions">
+                <a href="#" class="btn-profile">Edit Profile</a>
+                <a href="#" class="btn-icon" title="Public View">Public View</a>
+        </div>
+        <div class="overlay"></div>
+    </main>
     <?php require_once "modale_report.php" ?>
 </body>
 </html>
