a8b0743613e363da815d903405189a8a2d5002b4
Wayl Louaked <wlouaked@f6r12s4.clusters.42paris.fr>
2026-02-09 20:41:43 +0100

    Fix permission issue with avatars directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,12 +10,14 @@ sed -i "s/EMAILPASS_PLACEHOLDER/$EMAILPASS/g" /etc/msmtprc
 echo "Configuration des dossiers d'upload..."
 mkdir -p /var/www/html/public/uploads/posts
 mkdir -p /var/www/html/public/uploads/avatars
-
 # On donne la propriété à l'utilisateur d'Apache (www-data)
 chown -R www-data:www-data /var/www/html/public/uploads
-
 # Permissions (755 est suffisant et plus sécurisé que 777)
 chmod -R 755 /var/www/html/public/uploads
+
+# Permissions pour les avatars du setup
+chown -R www-data:www-data /var/www/html/assets/images/avatars
+chmod -R 755 /var/www/html/assets/images/avatars
 
 echo "Permissions OK."
 
