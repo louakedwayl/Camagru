0b6d71f8888d6afc395824a1f469dd63002b1381
Wayl Louaked <wlouaked@f6r12s3.clusters.42paris.fr>
2026-02-15 18:02:03 +0100

    Fix navbar.js: add null checks to prevent errors on visitor pages

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -2,35 +2,36 @@ const nav = document.querySelector('nav');
 const searchLink = document.querySelector('a#search-link');
 const searchBar = document.querySelector('#search-bar');
 const searchCross = document.querySelector('.search-cross');
-
 const notificationLink = document.querySelector('a#notification-link');
 const notificationBar = document.querySelector('#notification-bar');
 const notificationCross = document.querySelector('.notification-cross');
 
-// Empêcher le comportement par défaut du lien Search
-searchLink.addEventListener('click', function(e) {
-    e.preventDefault();
-    nav.style.display = "none";
-    searchBar.style.display = "flex";
-});
+if (searchLink) {
+    searchLink.addEventListener('click', function(e) {
+        e.preventDefault();
+        nav.style.display = "none";
+        searchBar.style.display = "flex";
+    });
+}
 
+if (searchCross) {
+    searchCross.addEventListener('click', () => {
+        searchBar.style.display = "none";
+        nav.style.display = "flex";
+    });
+}
 
-searchCross.addEventListener('click', ()=>
-{
-    searchBar.style.display = "none";
-    nav.style.display = "flex";
-});
+if (notificationLink) {
+    notificationLink.addEventListener('click', function(e) {
+        e.preventDefault();
+        nav.style.display = "none";
+        notificationBar.style.display = "flex";
+    });
+}
 
-
-notificationLink.addEventListener('click', function(e) {
-    e.preventDefault();
-    nav.style.display = "none";
-    notificationBar.style.display = "flex";
-});
-
-
-notificationCross.addEventListener('click', ()=>
-{
-    notificationBar.style.display = "none";
-    nav.style.display = "flex";
-});
+if (notificationCross) {
+    notificationCross.addEventListener('click', () => {
+        notificationBar.style.display = "none";
+        nav.style.display = "flex";
+    });
+}
