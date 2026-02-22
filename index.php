9275b0cd5571ee1aa74668c04641ecc06746af20
Wayl Louaked <wlouaked@f6r13s5.clusters.42paris.fr>
2026-02-22 11:40:45 +0100

    fix: disable browser cache for demo purge consistency

--- index.php
+++ index.php
@@ -7,7 +7,6 @@ require_once 'controllers/NotificationController.php';
 
 if (session_status() === PHP_SESSION_NONE) session_start();
 
-
 header("Cache-Control: no-cache, no-store, must-revalidate");
 header("Pragma: no-cache");
 header("Expires: 0");
