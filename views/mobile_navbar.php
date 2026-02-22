ca7adc5ff45618398b577f009c4d15482ced9b93
Wayl Louaked <wlouaked@f6r2s11.clusters.42paris.fr>
2026-02-22 16:37:43 +0100

    feat: mobile bottom navbar with home, search, explore, profile

--- views/mobile_navbar.php
+++ views/mobile_navbar.php
@@ -1,9 +1,25 @@
 <!-- MOBILE TOP BAR -->
 <div class="mobile-top-bar">
-    <a  href="index.php?action=create" >
-        <img src="assets/images/icon/create.svg" alt="create" class="create">
+    <a class="create" href="index.php?action=create">
+        <img src="assets/images/icon/create.svg" alt="create">
     </a>
-    <a href="#"  class="notification" onclick="toggleNotificationBar(); return false;">
+    <a class="notification" href="#" onclick="toggleNotificationBar(); return false;">
         <img src="assets/images/icon/heart.svg" alt="notifications">
+    </a>
+</div>
+
+<!-- MOBILE BOTTOM BAR -->
+<div class="mobile-bottom-bar">
+    <a href="index.php?action=home">
+        <img src="assets/images/icon/home.svg" alt="home">
+    </a>
+    <a href="#" id="mobile-search-link">
+        <img src="assets/images/icon/search.svg" alt="search">
+    </a>
+    <a href="index.php?action=explore">
+        <img src="assets/images/icon/compass.svg" alt="explore">
+    </a>
+    <a href="index.php?action=profile">
+        <img class="mobile-profile" src="assets/images/default-avatar.jpeg" alt="profile">
     </a>
 </div>
