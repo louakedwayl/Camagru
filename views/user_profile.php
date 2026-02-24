5cd0745677fda335dd9019638344734526ca2278
Wayl Louaked <wlouaked@f6r6s9.clusters.42paris.fr>
2026-02-24 20:11:37 +0100

    Add mobile navbar to user_profile.php

--- views/user_profile.php
+++ views/user_profile.php
@@ -8,10 +8,12 @@
     <link rel="apple-touch-icon" sizes="180x180" href="assets/images/favicon/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="assets/images/favicon/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicon/favicon-16x16.png">
+    <link rel="stylesheet" href="assets/css/mobile_navbar.css">
     <script defer src="assets/js/hamburger.js"></script>
     <script defer src="assets/js/user_profile.js"></script>
 </head>
 <body>
+    <?php require_once "mobile_navbar.php"; ?>
     <?php require_once __DIR__ . '/navbar.php'; ?>
     <main>
         <header>
